d23050f9f63bd4502b3c9570bded7f48527934b4
utsho-anik <anikdas548@gmail.com>
2023-06-18 08:49:23 +0600

    login done

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Session;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    public function halum(){
+        return "welcome";
+    }
+}
